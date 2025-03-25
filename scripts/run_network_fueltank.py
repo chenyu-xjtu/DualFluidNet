addd40b45a0d54761e8777f3c69cd48a2362557c
Yu <125364727+chenyu-xjtu@users.noreply.github.com>
2025-03-25 23:07:02 +0800

    refresh code

---
+++ scripts/run_network_fueltank.py
@@ -0,0 +1,313 @@
+#!/usr/bin/env python3
+import os
+os.environ['CUDA_VISIBLE_DEVICES']='1'
+import sys
+import argparse
+import numpy as np
+import re
+from glob import glob
+import time
+import importlib
+import json
+import dataflow
+import zstandard as zstd
+import msgpack
+import msgpack_numpy
+msgpack_numpy.patch()
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'datasets'))
+from physics_data_helper import numpy_from_bgeo, write_bgeo_from_numpy
+from create_physics_scenes import obj_surface_to_particles, obj_volume_to_particles
+import open3d as o3d
+
+class PhysicsSimDataFlow(dataflow.RNGDataFlow):
+    """Data flow for msgpacks generated from SplishSplash simulations.
+    """
+
+    def __init__(self, files, random_rotation=False, shuffle=False, window=2):
+        if not len(files):
+            raise Exception("List of files must not be empty")
+        if window < 1:
+            raise Exception("window must be >=1 but is {}".format(window))
+        self.files = files
+        self.random_rotation = random_rotation
+        self.shuffle = shuffle
+        self.window = window
+
+    def __iter__(self):
+        decompressor = zstd.ZstdDecompressor()
+        files_idxs = np.arange(len(self.files)) # files中train3200个.msgpack.zst文件, test320个.msgpack.zst文件。
+        if self.shuffle:
+            self.rng.shuffle(files_idxs) # 打乱帧的顺序
+
+        for file_i in files_idxs: # 对每一个.msgpack.zst文件进行遍历
+            # read all data from file
+            with open(self.files[file_i], 'rb') as f:
+                data = msgpack.unpackb(decompressor.decompress(f.read()),
+                                       raw=False) #一个.msgpack.zst中的数据：50帧的流体粒子数据fram_id, scene_id, pos, vel, m, viscosity，第一帧还包含box数据box, box_normal
+
+            data_idxs = np.arange(len(data) - self.window + 1) #windows=3, data_idxs:0-48
+            if self.shuffle:
+                self.rng.shuffle(data_idxs)
+
+            # get box from first item. The box is valid for the whole file
+            box = data[0]['box']
+            box_normals = data[0]['box_normals']
+
+            for data_i in data_idxs:
+            #对当前的.msgpack.zst里的每一帧进行遍历
+                if self.random_rotation:
+                    angle_rad = self.rng.uniform(0, 2 * np.pi)
+                    s = np.sin(angle_rad)
+                    c = np.cos(angle_rad)
+                    rand_R = np.array([c, 0, s, 0, 1, 0, -s, 0, c],
+                                      dtype=np.float32).reshape((3, 3))
+
+                if self.random_rotation:
+                    sample = {
+                        'box': np.matmul(box, rand_R),
+                        'box_normals': np.matmul(box_normals, rand_R)
+                    } # 给每一帧的盒子施加随机旋转（一帧一次）
+                else:
+                    sample = {'box': box, 'box_normals': box_normals}
+
+                for time_i in range(self.window): #为每一帧数据加上后两帧数据（包含三帧的数据）
+
+                    item = data[data_i + time_i]
+
+                    for k in ('pos', 'vel'):
+                        if self.random_rotation:
+                            sample[k + str(time_i)] = np.matmul(item[k], rand_R) # 给每一帧的粒子施加随机旋转（一个帧一次） 【给粒子和盒子同时施加旋转相当于把整个场景都旋转(即旋转重力)】
+                        else:
+                            sample[k + str(time_i)] = item[k]
+
+                    for k in ('m', 'viscosity', 'frame_id', 'scene_id'):
+                        sample[k + str(time_i)] = item[k]
+
+                yield sample #(最终每一帧有3帧的数据（pos,vel,m,viscosity,frame_id,scene_id）以及box和box_normal，共20个键值对)
+
+def write_particles(path_without_ext, pos, vel=None, options=None):
+    """Writes the particles as point cloud ply.
+    Optionally writes particles as bgeo which also supports velocities.
+    """
+    arrs = {'pos': pos}
+    if not vel is None:
+        arrs['vel'] = vel
+    np.savez(path_without_ext + '.npz', **arrs)
+
+    if options and options.write_ply:
+        pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(pos))
+        o3d.io.write_point_cloud(path_without_ext + '.ply', pcd)
+
+    if options and options.write_bgeo:
+        write_bgeo_from_numpy(path_without_ext + '.bgeo', pos, vel)
+
+def run_sim_torch(weights_path, scene, num_steps,
+                  output_dir, options, frames):
+    # import torch
+    # device = torch.device(options.device)
+
+    # prepare static particles
+    walls = []
+
+    for x in scene['walls']:
+        points = frames[0]['box'][0].copy()
+        normals = frames[0]['box_normals'][0].copy()
+        if 'invert_normals' in x and x['invert_normals']:
+            normals = -normals
+        points += np.asarray([x['translation']], dtype=np.float32)
+        walls.append((points, normals))
+    box = np.concatenate([x[0] for x in walls], axis=0)
+    box_normals = np.concatenate([x[1] for x in walls], axis=0)
+
+    # export static particles
+    write_particles(os.path.join(output_dir, 'box'), box, box_normals, options)
+
+    # compute lowest point for removing out of bounds particles
+    # min_y = np.min(box[:, 1]) - 0.05 * (np.max(box[:, 1]) - np.min(box[:, 1]))
+    # min_x = np.min(box[:, 0])
+    # max_x = np.max(box[:, 0])
+    # min_y = np.min(box[:, 1])
+    # max_y = np.max(box[:, 1])
+    # min_z = np.min(box[:, 2])
+    # max_z = np.max(box[:, 2])
+    # box = torch.from_numpy(box).to(device)
+    # box_normals = torch.from_numpy(box_normals).to(device)
+
+    # # prepare fluids
+    # fluids = []
+    # for x in scene['fluids']:
+    #     points = frame['pos0'][0].copy()
+    #     points += np.asarray([x['translation']], dtype=np.float32)
+    #     velocities = np.empty_like(points)
+    #     velocities[:, 0] = x['velocity'][0]
+    #     velocities[:, 1] = x['velocity'][1]
+    #     velocities[:, 2] = x['velocity'][2]
+    #     range_ = range(x['start'], x['stop'], x['step'])
+    #     fluids.append(
+    #         (points.astype(np.float32), velocities.astype(np.float32), range_))
+    #
+    # pos = np.empty(shape=(0, 3), dtype=np.float32)
+    # vel = np.empty_like(pos)
+
+    for step in range(num_steps):
+        # add from fluids to pos vel arrays
+        # for points, velocities, range_ in fluids:
+        #     if step in range_:  # check if we have to add the fluid at this point in time
+        #         pos = np.concatenate([pos, points], axis=0)
+        #         vel = np.concatenate([vel, velocities], axis=0)
+        pos = frames[step]['pos0'][0]
+        vel = frames[step]['vel0'][0]
+        if pos.shape[0]:
+            fluid_output_path = os.path.join(output_dir,
+                                             'fluid_{0:04d}'.format(step))
+            if isinstance(pos, np.ndarray):
+                write_particles(fluid_output_path, pos, vel, options)
+            else:
+                write_particles(fluid_output_path, pos.numpy(), vel.numpy(),
+                                options)
+
+        #     inputs = (torch.from_numpy(pos).to(device),
+        #               torch.from_numpy(vel).to(device), None, box, box_normals)
+        #     pos, vel = model(inputs)
+        #     pos = pos.cpu().numpy()
+        #     vel = vel.cpu().numpy()
+
+        # remove out of bounds particles
+        if step % 10 == 0:
+            print(step, 'num particles', pos.shape[0])
+            # mask = pos[:, 1] > min_y
+            # mask = np.logical_and.reduce((
+            #     pos[:, 0] >= min_x,
+            #     pos[:, 0] <= max_x,
+            #     pos[:, 1] >= min_y,
+            #     pos[:, 1] <= max_y,
+            #     pos[:, 2] >= min_z,
+            #     pos[:, 2] <= max_z
+            # ))
+            # if np.count_nonzero(mask) < pos.shape[0]:
+            #     pos = pos[mask]
+            #     vel = vel[mask]
+
+def read_data_val(files, **kwargs):
+    return read_data(files=files,
+                     batch_size=1,
+                     repeat=False,
+                     shuffle_buffer=None,
+                     num_workers=1,
+                     **kwargs)
+
+def read_data(files=None,
+              batch_size=1,
+              window=2,
+              random_rotation=False,
+              repeat=False,
+              shuffle_buffer=None,
+              num_workers=1,
+              cache_data=False):
+    print(files[0:20], '...' if len(files) > 20 else '')
+
+    # caching makes only sense if the data is finite
+    if cache_data:
+        if repeat == True:
+            raise Exception("repeat must be False if cache_data==True")
+        if random_rotation == True:
+            raise Exception("random_rotation must be False if cache_data==True")
+        if num_workers != 1:
+            raise Exception("num_workers must be 1 if cache_data==True")
+
+    df = PhysicsSimDataFlow(
+        files=files,
+        random_rotation=random_rotation,
+        shuffle=True if shuffle_buffer else False,
+        window=window,
+    )
+
+    if repeat:
+        df = dataflow.RepeatedData(df, -1)
+
+    if shuffle_buffer:
+        df = dataflow.LocallyShuffleData(df, shuffle_buffer)
+
+    if num_workers > 1:
+        df = dataflow.MultiProcessRunnerZMQ(df, num_proc=num_workers)
+
+    df = dataflow.BatchData(df, batch_size=batch_size, use_list=True)
+
+    if cache_data:
+        df = dataflow.CacheData(df)
+
+    df.reset_state()
+    return df
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=
+        "Runs a fluid network on the given scene and saves the particle positions as npz sequence",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # parser.add_argument("trainscript",
+    #                     type=str,
+    #                     help="The python training script.")
+    parser.add_argument(
+        "--weights",
+        type=str,
+        required=True,
+        help=
+        "The path to the .h5 network weights file for tensorflow ot the .pt weights file for torch."
+    )
+    parser.add_argument("--num_steps",
+                        type=int,
+                        default=250,
+                        help="The number of simulation steps. Default is 250.")
+    parser.add_argument("--scene",
+                        type=str,
+                        required=True,
+                        help="A json file which describes the scene.")
+    parser.add_argument("--output",
+                        type=str,
+                        required=True,
+                        help="The output directory for the particle data.")
+    parser.add_argument("--write-ply",
+                        action='store_true',
+                        help="Export particle data also as .ply sequence")
+    parser.add_argument("--write-bgeo",
+                        action='store_true',
+                        help="Export particle data also as .bgeo sequence")
+    parser.add_argument("--device",
+                        type=str,
+                        default='cuda',
+                        help="The device to use. Applies only for torch.")
+    parser.add_argument("--zst_dir",
+                        type=str,
+                        default='/home/wny/code/dual_small/scenes/fuel_triangletank/zst/test/rotate_test',
+                        help="Path to zst directory.")
+
+
+    args = parser.parse_args()
+    print(args)
+    dataset_dir = args.zst_dir
+
+    # module_name = os.path.splitext(os.path.basename(args.trainscript))[0]
+    sys.path.append('.')
+    # trainscript_module = importlib.import_module(module_name)
+
+    val_files = sorted(glob(os.path.join(dataset_dir, '*.zst')))
+    val_dataset = read_data_val(files=val_files, window=1, cache_data=True)
+    frames = []
+    for data in val_dataset:
+        if data['scene_id0'][0] == 'sim_0060':
+            frames.append(data)
+
+    with open(args.scene, 'r') as f:
+        scene = json.load(f)
+
+    os.makedirs(args.output)
+    if frames == []:
+        print("No frame")
+        return
+
+    return run_sim_torch(args.weights, scene,
+                             args.num_steps, args.output, args, frames)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ scripts/train_network_torch.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python3
+import importlib
+import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+import numpy as np
+import sys
+import argparse
+import yaml
+from torch.backends import cudnn
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from datasets.dataset_reader_physics_random_gravity import read_data_train, read_data_val
+from collections import namedtuple
+import glob
+import time
+import torch
+from utils.deeplearningutilities.torch import Trainer, MyCheckpointManager
+from evaluate_network import evaluate_torch, evaluate_whole_sequence_torch
+
+_k = 1000
+device_ids = [0, 1]
+TrainParams = namedtuple('TrainParams', ['max_iter', 'base_lr', 'batch_size'])
+train_params = TrainParams(60 * _k, 0.001, 2)
+# min_err = 0.00050
+# min_err_seq = 0.0306
+def create_model(**kwargs):
+    from models.default_torch import MyParticleNetwork
+    """Returns an instance of the network for training and evaluation"""
+    model = MyParticleNetwork(**kwargs)
+    return model
+
+
+def main():
+    global min_err
+    parser = argparse.ArgumentParser(description="Training script")
+    parser.add_argument(  "cfg",
+                        type=str,
+                        help="The path to the yaml config file")
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    args = parser.parse_args()
+
+    with open(args.cfg, 'r') as f:
+        cfg = yaml.safe_load(f)
+
+    # the train dir stores all checkpoints and summaries. The dir name is the name of this file combined with the name of the config file
+    train_dir = os.path.splitext(
+        os.path.basename(__file__))[0] + '_' + os.path.splitext(
+            os.path.basename(args.cfg))[0]
+
+    val_files = sorted(glob.glob(os.path.join(cfg['dataset_dir'], 'valid', '*.zst'))) #(20个场景序列，每个序列15帧，300帧)
+    train_files = sorted(
+        glob.glob(os.path.join(cfg['dataset_dir'], 'train', '*.zst'))) #(200个场景序列，每个序列15帧，共3000帧)
+
+    device = torch.device("cuda")
+
+    val_dataset = read_data_val(files=val_files, window=1, cache_data=True)
+
+    dataset = read_data_train(files=train_files,
+                              batch_size=train_params.batch_size,
+                              window=3,
+                              num_workers=2,
+                              **cfg.get('train_data', {}))
+
+    data_iter = iter(dataset) # 用iter迭代器来遍历dataset，iter通过next来遍历每一个batch
+
+    trainer = Trainer(train_dir)
+
+    model = create_model(**cfg.get('model', {}))
+    # if torch.cuda.device_count() > 1:
+    #     print("Let's use", torch.cuda.device_count(), "GPUs!")
+    #     model = torch.nn.DataParallel(model, device_ids=[0,1])
+
+    model.cuda()
+
+    boundaries = [
+        15 * _k,
+        25 * _k,
+        35 * _k,
+        45 * _k,
+        50 * _k,
+        55 * _k
+    ]
+    # boundaries = [
+    #     50 * _k,
+    #     60 * _k,
+    #     70 * _k,
+    #     80 * _k,
+    #     100 * _k
+    # ]
+    lr_values = [
+        1.5,
+        1,
+        0.5,
+        0.25,
+        0.125,
+        0.125 * 0.5
+    ]
+
+    def lrfactor_fn(x):
+        factor = lr_values[0]
+        for b, v in zip(boundaries, lr_values[1:]):
+            if x > b:
+                factor = v
+            else:
+                break
+        return factor
+
+    optimizer = torch.optim.Adam(model.parameters(),
+                                 lr=train_params.base_lr,
+                                 weight_decay=0.001,
+                                 eps=1e-6)
+    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lrfactor_fn)
+
+    step = torch.tensor(0)
+    checkpoint_fn = lambda: {
+        'step': step,
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'scheduler': scheduler.state_dict()
+    }
+
+    manager = MyCheckpointManager(checkpoint_fn,
+                                  trainer.checkpoint_dir,
+                                  keep_checkpoint_steps=list(
+                                      range(1 * _k, train_params.max_iter + 1,
+                                            1 * _k)))
+
+    def euclidean_distance(a, b, epsilon=1e-9):
+        return torch.sqrt(torch.sum((a - b)**2, dim=-1) + epsilon)
+
+    def loss_fn(pr_pos, gt_pos, num_fluid_neighbors):
+        gamma = 0.5
+        neighbor_scale = 1 / 40
+        importance = torch.exp(-neighbor_scale * num_fluid_neighbors)
+        return torch.mean(importance *
+                          euclidean_distance(pr_pos, gt_pos)**gamma)
+
+    def train(model, batch):
+        optimizer.zero_grad()
+        losses = []
+
+        batch_size = train_params.batch_size
+        for batch_i in range(batch_size):
+            inputs = ([
+                batch['pos0'][batch_i], batch['vel0'][batch_i], None,
+                batch['box'][batch_i], batch['box_normals'][batch_i]
+            ])
+
+            #第一帧
+            pr_pos1, pr_vel1 = model(inputs)
+
+            l = 0.5 * loss_fn(pr_pos1, batch['pos1'][batch_i],
+                              model.num_fluid_neighbors)
+
+            inputs = (pr_pos1, pr_vel1, None, batch['box'][batch_i],
+                      batch['box_normals'][batch_i])
+            #第二帧
+            pr_pos2, pr_vel2 = model(inputs)
+
+            l += 0.5 * loss_fn(pr_pos2, batch['pos2'][batch_i],
+                               model.num_fluid_neighbors) # 两帧损失
+            losses.append(l)
+
+        total_loss = 128 * sum(losses) / batch_size
+        total_loss.backward()
+        optimizer.step() #一个batch更新一次
+
+        return total_loss
+
+    if manager.latest_checkpoint:
+        print('restoring from ', manager.latest_checkpoint)
+        latest_checkpoint = torch.load(manager.latest_checkpoint)
+        step = latest_checkpoint['step']
+        model.load_state_dict(latest_checkpoint['model'])
+        optimizer.load_state_dict(latest_checkpoint['optimizer'])
+        scheduler.load_state_dict(latest_checkpoint['scheduler'])
+
+    display_str_list = []
+    while trainer.keep_training(step,
+                                train_params.max_iter, #最大迭代次数50000
+                                checkpoint_manager=manager,
+                                display_str_list=display_str_list):
+        # 每次循环一个batch
+        # 所以总共迭代帧应该是max_iter * batch_size
+        # 通过不断迭代数据集直到停止，没有分多少个epoch，而是通过iter一直循环下去。 这里的iter()数据遍历完会继续重复
+        data_fetch_start = time.time()
+        batch = next(data_iter) # iter通过next来遍历每一个batch，其中一个batch有16(batch_size)组场景数据，每组数据包含20个属性，如pos1，vel1(第一帧gt)，pos2，vel2(第二帧gt)等 【每组数据只有两帧】
+
+        batch_torch = {}
+        for k in ('pos0', 'vel0', 'pos1', 'pos2', 'box', 'box_normals'):
+            # batch_torch每组数据包含6个属性：pos0, vel0, pos1, pos2, box, box_normals
+            batch_torch[k] = [torch.from_numpy(x).to(device) for x in batch[k]]
+        data_fetch_latency = time.time() - data_fetch_start #数据延迟：存储或检索数据包所需的时间
+        trainer.log_scalar_every_n_minutes(5, 'DataLatency', data_fetch_latency)
+
+        current_loss = train(model, batch_torch)
+        #batch_torch(n, bs, p,3) n是n种属性（作键），bs是batch_size，p是该组场景数据中的点数（各场景点数不相同），3是特征维度为三维（xyz）
+        scheduler.step()
+        display_str_list = ['loss', float(current_loss)] #记录每一次迭代的loss
+
+        if trainer.current_step % 10 == 0:
+            trainer.summary_writer.add_scalar('TotalLoss', current_loss,
+                                              trainer.current_step)
+            trainer.summary_writer.add_scalar('LearningRate',
+                                              scheduler.get_last_lr()[0],
+                                              trainer.current_step)
+        # 每10个iteration打印一次loss
+
+        if (trainer.current_step) % (1.0 * _k) == 0:
+            for k, v in evaluate_torch(model,
+                                 val_dataset,
+                                 frame_skip=20,
+                                 device=device,
+                                 **cfg.get('evaluation', {})).items():
+                trainer.summary_writer.add_scalar('eval/' + k, v,
+                                                  trainer.current_step)
+                if(k == "err_n1" and v < min_err):
+                    min_err = v
+                    torch.save({'model': model.state_dict()}, str(step.item())+'_model_weights_best.pt')
+                    print("=================update best model: err_n1=" + str(v) + "===============")
+
+    # 第1000次迭代eval一次
+
+    torch.save({'model': model.state_dict()}, 'model_weights.pt') #所有batch遍历一遍记录下当前model
+    if trainer.current_step == train_params.max_iter:
+        return trainer.STATUS_TRAINING_FINISHED
+    else:
+        return trainer.STATUS_TRAINING_UNFINISHED
+
+
+if __name__ == '__main__':
+    import multiprocessing as mp
+    mp.set_start_method('spawn')
+    sys.exit(main())
